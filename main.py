2a6b6945a5d46267c37b707daf0593f408cd7d4f
Dekel321 <85648677+Dekel321@users.noreply.github.com>
2024-03-25 01:24:25 +0000

    main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def num1():
     filetype = input("What Is This Password For: ")
     acctype = input("What Nickname Would You Give This Acc: ")
     create_file(filetype)
+    file_path = file_exist()
 
     fread = open(str(Path(filetype) / (acctype + ".txt")), "w")
     Username = input("Enter Username: ")
@@ -15,14 +16,14 @@ def num1():
 
 
 def num2():
-    location = "C:/Users/Dekel/Documents/Visual Studio project/python project"
-    files_dir = listfiles(location)
-    
-    for file_name in files_dir:
-        print(file_name) 
-
-
-    
+    file_path = file_exist()
+    try:
+        with open(file_path, "r") as file:
+            file_contents = file.read()
+            print(file_contents)
+            exit()
+    except Exception as e:
+        print(f"Error: {e}")
     
 
 def num3():
@@ -38,6 +39,7 @@ def opt(option):
     else:
         print("Only Use Numbers 1 - 3")
 
+
 def create_file(folder):
     try:
         Path(folder).mkdir()
@@ -47,6 +49,7 @@ def create_file(folder):
     except Exception as e:
         print(f"Failed to create directory: {e}")
         exit()
+
 
 def listfiles(directory):
     file_list = []  
@@ -62,7 +65,31 @@ def listfiles(directory):
     return file_list
 
 
+def file_exist():
+    location = "C:/Users/Dekel/Documents/Visual Studio project/python project/passtest"
+    files_dir = listfiles(location)
 
+    for file_name in files_dir:
+        print(file_name) 
+
+    open_file = input("Witch File Would You Like To Open: ")
+    the_path = location + "/" + open_file
+
+    if len(open_file) >= 3:
+        last3 = open_file[-4:]
+
+
+
+    if '.' in last3 and not last3 == ".txt":
+        print("i only take .txt files sorry")
+        exit()
+    elif not last3 == '.txt':
+        the_path += ".txt"
+        
+    return the_path
+ 
+        
+  
 
 print("1 Save: \n2 Read: \n3 Change:")
 option = 0
@@ -74,5 +101,3 @@ while not option == 1 or option == 2 or option == 3:
         print("Cant Use Words")
 
         
-        
-
