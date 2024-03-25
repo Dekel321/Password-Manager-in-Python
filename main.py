b088cbaac8daf6c2d73306e2f33bb6f8bbca8d30
Dekel321 <85648677+Dekel321@users.noreply.github.com>
2024-03-25 21:21:53 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 from pathlib import Path
 
+location = "C:/Users/Dekel/Documents/Visual Studio project/python project/Passwords"
 
 def num1():
-    filetype = input("What Is This Password For: ")
-    acctype = input("What Nickname Would You Give This Acc: ")
-    create_file(filetype)
-    file_path = file_exist()
+    
+    folder_name = input("What Is This Password For: ")
+    com_path_one = Path(location) / (folder_name)
+    if com_path_one.exists():
+        pass
+    else: 
+        create_file(Path(location) / (folder_name))
+        print("created")
 
-    fread = open(str(Path(filetype) / (acctype + ".txt")), "w")
+    account = input("What Nickname Would You Give This Acc: ")
+    com_path_to = Path(com_path_one) / (account)
+
+    d_type = typecheck(com_path_to)
+    
+    if  d_type == True:
+        com_path_to = str(com_path_to)  + ".txt"       
+    elif d_type == False:
+        print("Cant Save Your Passwords In That File Type \nOR \nCant End With '.' In The Last 3 Letters")
+        exit()  
+    else:
+        print("some error just fix me")
+    
+    fread = open(com_path_to, "w")
     Username = input("Enter Username: ")
     Password = input("Enter Password: ")
     fread.write(Username + "\n" + Password)
@@ -16,9 +34,24 @@ def num1():
 
 
 def num2():
-    file_path = file_exist()
+    files_dir = list_files(location)
+
+    for file_name in files_dir:
+        print(file_name) 
+    
+    open_file = input("Witch File Would You Like To Open: ")
+    the_path = Path(location) / (open_file)
+
+    #check type
+    d_type = typecheck(the_path)
+    if d_type == True:
+        the_path = str(the_path)  + ".txt"
+    elif d_type == False:
+        print("file is not .txt")
+        exit()
+
     try:
-        with open(file_path, "r") as file:
+        with open(the_path, "r") as file:
             file_contents = file.read()
             print(file_contents)
             exit()
@@ -51,7 +84,7 @@ def create_file(folder):
         exit()
 
 
-def listfiles(directory):
+def list_files(directory):
     file_list = []  
     try:
         files = Path(directory).glob("*")
@@ -64,33 +97,21 @@ def listfiles(directory):
     
     return file_list
 
+def typecheck(path_to_file):
+    t_or_f = True
+    file_path = Path(path_to_file)
+    file_type = file_path.suffix
+    print(file_type)
+    if '.' in file_type and not file_type == ".txt":
 
-def file_exist():
-    location = "C:/Users/Dekel/Documents/Visual Studio project/python project/passtest"
-    files_dir = listfiles(location)
-
-    for file_name in files_dir:
-        print(file_name) 
-
-    open_file = input("Witch File Would You Like To Open: ")
-    the_path = location + "/" + open_file
-
-    if len(open_file) >= 3:
-        last3 = open_file[-4:]
-
-
-
-    if '.' in last3 and not last3 == ".txt":
-        print("i only take .txt files sorry")
-        exit()
-    elif not last3 == '.txt':
-        the_path += ".txt"
+        t_or_f = False
         
-    return the_path
- 
-        
+    elif file_type == ".txt":
+        t_or_f = True
+
+    return t_or_f
+
   
-
 print("1 Save: \n2 Read: \n3 Change:")
 option = 0
 while not option == 1 or option == 2 or option == 3:    
